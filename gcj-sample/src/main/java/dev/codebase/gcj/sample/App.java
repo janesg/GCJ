12990171c9bd4c076d3f093fab29329296663241
janesg <gary_janes@hotmail.com>
2015-07-20 15:03:05 +0100

    Extended the displayed message

---
+++ gcj-sample/src/main/java/dev/codebase/gcj/sample/App.java
@@ -0,0 +1,13 @@
+package dev.codebase.gcj.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! - Code is in Git" );
+    }
+}
