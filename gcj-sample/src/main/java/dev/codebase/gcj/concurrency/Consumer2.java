c7e1198f33fc48f0483bb178040e3cdddaafaa93
janesg <gary_janes@hotmail.com>
2015-08-11 07:56:48 +0100

    Concurrency examples

---
+++ gcj-sample/src/main/java/dev/codebase/gcj/concurrency/Consumer2.java
@@ -0,0 +1,24 @@
+package dev.codebase.gcj.concurrency;
+
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+
+public class Consumer2 implements Runnable {
+    private BlockingQueue<String> bq;
+
+    public Consumer2(BlockingQueue<String> bq) {
+        this.bq = bq;
+    }
+
+    public void run() {
+        Random random = new Random();
+        
+        try {
+            for (String message = bq.take(); !message.equals("DONE"); message = bq.take()) {
+                System.out.format("MESSAGE RECEIVED: %s%n", message);
+                Thread.sleep(random.nextInt(3000));
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+}
---
+++ gcj-sample/src/main/java/dev/codebase/gcj/concurrency/ProducerConsumerExample.java
@@ -0,0 +1,10 @@
+package dev.codebase.gcj.concurrency;
+
+public class ProducerConsumerExample {
+    public static void main(String[] args) {
+        Drop drop = new Drop();
+        
+        (new Thread(new Producer(drop))).start();
+        (new Thread(new Consumer(drop))).start();
+    }
+}
